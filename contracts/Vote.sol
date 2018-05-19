f69d98359ddf4d8b393b30a51c7d35a295e3d14e
stu-GYULA <1018757052@qq.com>
2018-05-20 02:16:35 +0800

    contract change

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -1,65 +1,36 @@
 pragma solidity^0.4.18;
 //pragma experimental ABIEncoderV2;
 
-import "./CreateVote.sol";
+import "./CreateVote1.sol";
 
 contract Vote is CreateVote{
     
+    //set of event
     event SucceedInVote(
         address sender,
         uint32 serial);
     
     event SucceedCheck(
         address sender,
-        uint32 serial);
+        uint32 serial,
+        uint32[20] array,
+        string voterToProposalName
+        );
         
-    //Í¶Æ±ÈË½á¹¹Ìå
+    //投票人结构体
     struct Voter{
         address voterAddress;
         bool state;
     }
 
-    //Í¶Æ±ÈËµ½Í¶Æ±ÈËÑ¡ÔñµÄÓ³Éä
+    //投票人到投票人选择的映射
     //mapping(address => string) voterToProposalName;
 
-    //´æ´¢ÒÑÍ¶¹ýÆ±µÄµØÖ·
+    //存储已投过票的地址
     //address[] allVoters;
     
-    // function vote
-    function toVote(uint32 serial, uint32 index, string proposalName) public returns(string result){
-        
-        require(serial>0);
-        assert(!verifySerial(serial));
-        assert(!verifyAddress(serial,msg.sender));
-        
-        toMyVote[serial].allVoters.push(msg.sender);
-        toMyVote[serial].voterToProposalName[msg.sender] = proposalName;
-        toMyVote[serial].count[index]++;
-        //count[i] = count[i]+1;
-        
-        emit SucceedInVote(msg.sender,serial);
-        
-        return "sucess!";
-        
-    }
-    
-    //check vote
-    function checkVote(uint32 serial) public  returns(uint32[10] result){
-        
-        require(serial>0);
-        assert(!verifySerial(serial));
-        assert(verifyAddress(serial,msg.sender));
-        
-        uint32[10] memory tmp ;
-        //tmp[0] =toMyVote[serial].count[0];
-        for (uint32 j=0;j<10;j++){
-            tmp[j] = toMyVote[serial].count[j];
-        }
-        
-        emit SucceedCheck(msg.sender,serial);
-        
-        return tmp;
-    }
+    //add sender's select to struct 
+    //function addSelect(uint32, serial)
     
     //verify address
     function verifyAddress(uint32 serial,address thisAddress) internal view returns(bool result){
@@ -72,7 +43,31 @@ contract Vote is CreateVote{
             }
         }
     }
-}
     
- 
+    // function vote
+    function toVote(uint32 serial, uint32 index, string proposalName) public {
+        
+        require(serial>0);
+        require(toMyVote[serial].state);
+        assert(!verifySerial(serial));
+        assert(!verifyAddress(serial,msg.sender));
+        
+        toMyVote[serial].allVoters.push(msg.sender);
+        toMyVote[serial].voterToProposalName[msg.sender] = proposalName;
+        toMyVote[serial].count[index]++;
+        //count[i] = count[i]+1;
+        
+        emit SucceedInVote(msg.sender,serial);
+    }
+    
+    //check vote
+    function checkVote(uint32 serial) public  {
+        
+        require(serial>0);
+        assert(!verifySerial(serial));
+        assert(verifyAddress(serial,msg.sender));
+        
+        emit SucceedCheck(msg.sender,serial,toMyVote[serial].count,toMyVote[serial].voterToProposalName[msg.sender]);
+    }
+}
     
